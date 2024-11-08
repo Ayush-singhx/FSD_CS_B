85d725feeb781445cfa767caa5e129b0c9e2b0b6
ayush singh <ayushsingh2081@gmail.com>
2024-11-08 16:25:44 +0530

    test

--- JS/fetch.js
+++
@@ -1,9 +0,0 @@
-const url="https://api.github.com/users/ayush-singhx";
-const res=fetch(url);
-res.then((res)=>{
-    return res.json();    
-}).then((data)=>{
-    console.log(data);
-}).catch((err)=>{
-    console.log("error",err);
-}).finally(()=>console.log("finally executed"));
--- JS/index.js
+++
@@ -1,6 +0,0 @@
-var a=12;
-if (a>10) {
-    a=30
-    console.log(a);
-}
-console.log(a);
